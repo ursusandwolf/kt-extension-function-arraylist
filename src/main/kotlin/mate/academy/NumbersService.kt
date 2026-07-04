b6fe9774fedf003bf39882f4ae34421aa19e9449
alex <ursusandwolf@protonmail.com>
2026-07-04 13:15:56 +0300

    Generalize ArrayList size extension

--- src/main/kotlin/mate/academy/NumbersService.kt
+++ src/main/kotlin/mate/academy/NumbersService.kt
@@ -1,3 +1,10 @@
 package mate.academy
 
-// provide your solution below
+fun main() {
+    val list : ArrayList<Int> = arrayListOf<Int>(10, 20, 30, 40, 50)
+    println(list.getSizeMessage())
+}
+
+fun <T> ArrayList<T>.getSizeMessage(): String {
+    return "The list contains ${this.size} elements."
+}
